085801f7bc0a3d27d46fd68a0fd2c7a6a0f4932d
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-06 02:25:25 +0530

    placeholder fo test given

---
+++ src/Screens/Dashboard/Components/TestGiven.js
@@ -0,0 +1,45 @@
+const TestGiven = () => {
+  let myList = ["bg-pink-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
+  return (
+    <>
+      <div className="inline-block p-4 m-2 shadow-xl border border-black rounded-xl bg-white">
+        <div>
+          <p className="font-bold">Tests Given</p>
+        </div>
+        <div className="grid grid-cols-2 gap-1 p-2">
+          <div
+            className={`${myList.at(
+              0
+            )} w-32 h-32 flex items-center justify-center`}
+          >
+            <p>Test1</p>
+          </div>
+
+          <div
+            className={`${myList.at(
+              1
+            )}  w-32 h-32 flex items-center justify-center`}
+          >
+            <p>Test2</p>
+          </div>
+          <div
+            className={`${myList.at(
+              2
+            )}  w-32 h-32 flex items-center justify-center`}
+          >
+            <p>Test3</p>
+          </div>
+          <div
+            className={`${myList.at(
+              3
+            )}  w-32 h-32 flex items-center justify-center`}
+          >
+            <p>Test4</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TestGiven;
