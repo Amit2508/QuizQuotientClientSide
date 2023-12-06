3d141f1c8dc8c26d1edace4bc7d546e4bca6007f
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-06 18:39:59 +0530

    Some CSS changes

--- src/Screens/Dashboard/Components/TestGiven.js
+++ src/Screens/Dashboard/Components/TestGiven.js
@@ -1,40 +1,43 @@
 const TestGiven = () => {
-  let myList = ["bg-pink-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
+  let myList = ["bg-blue-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
   return (
     <>
-      <div className="inline-block p-4 m-2 shadow-xl border border-black rounded-xl bg-white">
+      <div className="inline-block p-4 m-2 shadow-2xl  rounded-xl bg-white">
         <div>
           <p className="font-bold">Tests Given</p>
         </div>
-        <div className="grid grid-cols-2 gap-1 p-2">
+        <div className="grid grid-cols-2 gap-12 p-2">
           <div
             className={`${myList.at(
               0
-            )} w-32 h-32 flex items-center justify-center`}
+            )} w-32 h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-blue-600 hover:shadow-blue-700 hover:cursor-pointer`}
           >
-            <p>Test1</p>
+            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <p className="z-10 font-bold text-white">Test1</p>
           </div>
-
           <div
             className={`${myList.at(
               1
-            )}  w-32 h-32 flex items-center justify-center`}
+            )} w-32 h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-red-500 hover:shadow-red-600 hover:cursor-pointer`}
           >
-            <p>Test2</p>
+            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <p className="z-10 font-bold text-white">Test2</p>
           </div>
           <div
             className={`${myList.at(
               2
-            )}  w-32 h-32 flex items-center justify-center`}
+            )} w-32 h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-pink-400 hover:shadow-pink-500 hover:cursor-pointer`}
           >
-            <p>Test3</p>
+            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <p className="z-10 font-bold text-white">Test3</p>
           </div>
           <div
             className={`${myList.at(
               3
-            )}  w-32 h-32 flex items-center justify-center`}
+            )} w-32 h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-orange-500 hover:shadow-orange-600 hover:cursor-pointer`}
           >
-            <p>Test4</p>
+            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <p className="z-10 font-bold text-white">Test4</p>
           </div>
         </div>
       </div>
--- src/Screens/Dashboard/Components/TestUpcoming.js
+++ src/Screens/Dashboard/Components/TestUpcoming.js
@@ -2,25 +2,26 @@ const TestUpcoming = () => {
   let myList = ["bg-pink-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
   return (
     <>
-      <div className="inline-block p-4 m-2 shadow-xl border border-black rounded-xl bg-white">
+      <div className="inline-block p-4 m-2 shadow-xl rounded-xl bg-white">
         <div>
           <p className="font-bold">Tests Upcoming</p>
         </div>
-        <div className="grid grid-cols-1 gap-1 p-2 w-64">
+        <div className="grid grid-cols-1 gap-12 p-2 w-64">
           <div
             className={`${myList.at(
               0
-            )} w-auto h-32 flex items-center justify-center`}
+            )} h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-pink-600 hover:shadow-pink-700 hover:cursor-pointer`}
           >
-            <p>Test1</p>
+            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <p className="z-10 font-bold text-white">Test1</p>
           </div>
-
           <div
             className={`${myList.at(
               1
-            )}  w-auto h-32 flex items-center justify-center`}
+            )} h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-red-500 hover:shadow-red-600 hover:cursor-pointer`}
           >
-            <p>Test2</p>
+            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <p className="z-10 font-bold text-white">Test2</p>
           </div>
         </div>
       </div>
