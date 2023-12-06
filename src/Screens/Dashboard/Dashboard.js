dda61b2c5a9564fa220be068bd596221a7da6abd
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-06 18:39:43 +0530

    Added Footer

--- src/Screens/Dashboard/Dashboard.js
+++ src/Screens/Dashboard/Dashboard.js
@@ -2,24 +2,33 @@ import Navbar from "./Components/Navbar";
 import SubNavbar from "./Components/SubNavbar";
 import TestGiven from "./Components/TestGiven";
 import TestUpcoming from "./Components/TestUpcoming";
+import Footer from "./Components/Footer";
 const Dashboard = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <div>
         <Navbar />
       </div>
       <div>
         <SubNavbar />
       </div>
-      <div className="flex flex-col sm:justify-evenly bg-sky-100 h-32 sm:flex sm:flex-row">
-        <div>
+      <div
+        className="flex flex-col sm:flex sm:flex-row flex-grow"
+        style={{
+          background: "linear-gradient(to bottom, #e0f2fe 50%, white 50%)",
+        }}
+      >
+        <div className="flex-grow">
           <TestGiven />
         </div>
-        <div>
+        <div className="flex-grow">
           <TestUpcoming />
         </div>
       </div>
-    </>
+      <div className="mt-12">
+        <Footer />
+      </div>
+    </div>
   );
 };
 export default Dashboard;
