086420b71bcd39bece0f51ae38760668624e4f32
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-07 15:35:10 +0530

    Added dark mode css

--- src/Screens/Dashboard/Components/TestGiven.js
+++ src/Screens/Dashboard/Components/TestGiven.js
@@ -1,10 +1,33 @@
-const TestGiven = () => {
+import { useEffect, useState } from "react";
+
+const TestGiven = ({ getState }) => {
   let myList = ["bg-blue-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
+  const [state, setState] = useState(getState);
+  const [bgColor, setbgColor] = useState("bg-white");
+  const [text, setText] = useState("text-black");
+  const [shadow, setShadow] = useState("shadow-sky-500")
+  useEffect(() => {
+    setState(getState);
+  }, [getState]);
+
+  useEffect(() => {
+    if (state === 0) {
+      setbgColor("bg-white");
+      setText("text-black");
+      setShadow("shadow-sky-500")
+    } else if (state === 1) {
+      setbgColor("bg-black");
+      setText("text-white");
+      setShadow("shadow-purple-500")
+    }
+  }, [state]);
   return (
     <>
-      <div className="inline-block p-4 m-2 shadow-2xl shadow-sky-500 rounded-xl bg-white">
+      <div
+        className={`inline-block p-4 m-2 shadow-2xl ${shadow} rounded-xl ${bgColor}`}
+      >
         <div>
-          <p className="font-bold">Tests Given</p>
+          <p className={`font-bold ${text}`}>Tests Given</p>
         </div>
         <div className="grid grid-cols-2 gap-12 p-2">
           <div
