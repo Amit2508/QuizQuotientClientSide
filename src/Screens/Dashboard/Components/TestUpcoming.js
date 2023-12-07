fc72b0e18460f8866ad64b65d6891b0835010aec
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-07 15:35:14 +0530

    Added dark mode css

--- src/Screens/Dashboard/Components/TestUpcoming.js
+++ src/Screens/Dashboard/Components/TestUpcoming.js
@@ -1,10 +1,31 @@
-const TestUpcoming = () => {
+import { useEffect, useState } from "react";
+
+const TestUpcoming = ({ getState }) => {
   let myList = ["bg-pink-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
+  const [state, setState] = useState(getState);
+  const [bgColor, setbgColor] = useState("bg-white");
+  const [text, setText] = useState("text-black");
+  const [shadow, setShadow] = useState("shadow-sky-500")
+  useEffect(() => {
+    setState(getState);
+  }, [getState]);
+
+  useEffect(() => {
+    if (state === 0) {
+      setbgColor("bg-white");
+      setText("text-black");
+      setShadow("shadow-sky-500")
+    } else if (state === 1) {
+      setbgColor("bg-black");
+      setText("text-white");
+      setShadow("shadow-purple-500")
+    }
+  }, [state]);
   return (
     <>
-      <div className="inline-block p-4 m-2 shadow-xl rounded-xl shadow-sky-500 bg-white">
+      <div className={`inline-block p-4 m-2 shadow-xl rounded-xl ${shadow} ${bgColor}`}>
         <div>
-          <p className="font-bold">Tests Upcoming</p>
+          <p className={`font-bold ${text}`}>Tests Upcoming</p>
         </div>
         <div className="grid grid-cols-1 gap-12 p-2 w-64">
           <div
