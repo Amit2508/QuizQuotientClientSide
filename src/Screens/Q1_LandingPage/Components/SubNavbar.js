18bc164791667a72e44076ac8f7f25f7b2b092c5
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-11 21:32:06 +0530

    Implemented the SubNavbar for the landing screen

--- src/Screens/Q1_LandingPage/Components/SubNavbar.js
+++ src/Screens/Q1_LandingPage/Components/SubNavbar.js
@@ -2,9 +2,9 @@ import BackGround1 from "../../Icons/vector-5.svg";
 import actor from "../../Icons/actor1.png";
 import { useState, useEffect } from "react";
 
-const SubNavbar = ({getState}) => {
+const SubNavbar = ({ getState }) => {
   const [bg, setBg] = useState("bg-steelblue-200");
-  const [bg1, setBg1] = useState("text-black")
+  const [bg1, setBg1] = useState("text-blue-400");
   const [state, setState] = useState(getState);
 
   useEffect(() => {
@@ -14,42 +14,45 @@ const SubNavbar = ({getState}) => {
   useEffect(() => {
     if (state === 0) {
       setBg("bg-steelblue-200");
-      setBg1("text-black")
+      setBg1("text-blue-400");
     } else if (state === 1) {
       setBg("bg-slate-800");
-      setBg1("text-white")
+      setBg1("text-white");
     }
-
   }, [state]);
 
   return (
     <>
-      <div className={`absolute w-full`}>
-        <div
-          className={`${bg} text-left text-2xl text-black font-base flex items-center justify-center z-10 sm:h-screen`}
-        >
-          <img src={BackGround1} alt={`Background`} className="h-full" />
-        </div>
-        <div className={`absolute w-full h-full inset-0 z-20`}>
-          <div className={`flex justify-evenly items-center p-4`}>
-            <div>
-              <p
-                className={`sm:text-5xl text-2xl ${bg1} rounded-t-2xl p-2 bg-opacity-40 font-bold `}
-              >
-                Welcome to Quiz Quotient
-              </p>
-              <p
-                className={`sm:text-3xl text-lg ${bg1} rounded-b-2xl p-2 bg-opacity-40 font-bold `}
-              >
-                An interactive and fun way to participate in quiz
-              </p>
-            </div>
-            <div className={``}>
-              <img src={actor} alt={`actor`} className={`sm:h-screen h-84`} />
+        <div className="relative w-full">
+          <div
+            className={`${bg} text-left text-2xl text-black font-base flex items-center justify-center z-10 sm:h-full`}
+          >
+            <img src={BackGround1} alt={`Background`} className="h-full" />
+          </div>
+          <div className={`absolute w-full h-full inset-0 z-20`}>
+            <div className={`flex justify-evenly items-center p-4`}>
+              <div>
+                <p
+                  className={`sm:text-5xl text-2xl ${bg1} rounded-t-2xl p-2 bg-opacity-40 font-bold `}
+                >
+                  Welcome to Quiz Quotient
+                </p>
+                <p
+                  className={`sm:text-3xl text-lg ${bg1} rounded-b-2xl p-2 bg-opacity-40 font-bold `}
+                >
+                  An interactive and fun way to participate in quiz
+                </p>
+                <div className={`flex sm:flex-row flex-col justify-evenly`}>
+                  <button className={`sm:p-3 p-1 bg-pink-500 rounded-xl font-bold text-white`}>Log In</button>
+                  <button className={`sm:p-3 p-1 bg-purple-500 rounded-xl font-bold text-white mt-2 sm:mt-0`}>Register</button>
+                </div>
+              </div>
+              <div className={`block`}>
+                <img src={actor} alt={`actor`} className={`-z-20`} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
     </>
   );
 };
