04f0a63fbdb23d4acca64180ef65f800b7acfeca
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-12 18:54:49 +0530

    Added the functionality for dynaminc navbar items

--- src/Screens/Components/Navbar.js
+++ src/Screens/Components/Navbar.js
@@ -1,19 +1,22 @@
 import userPhoto from "../Icons/profile.png";
 import bright from "../Icons/brightness.png";
 import night from "../Icons/night.png";
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = ({ updateState }) => {
+const Navbar = ({ updateState, screen }) => {
   let get_darknessState = localStorage.getItem("web_state");
   if (get_darknessState === null) {
     get_darknessState = 0;
   }
+  const navigate = useNavigate();
   const [darknessState, setDarknessState] = useState(
     parseInt(get_darknessState)
   );
   const [imageSet, setImageSet] = useState(bright);
   const [backGroundColor, setBackGroundColor] = useState("bg-white");
   const [text, setText] = useState("text-black");
+  const [items, setItems] = useState([]);
 
   const handleLightClickListener = () => {
     const newDarknessState = updateState(darknessState === 0 ? 1 : 0);
@@ -49,6 +52,21 @@ const Navbar = ({ updateState }) => {
     }
   }, []);
 
+  useEffect(() => {
+    if (screen === "Landing") {
+      const LandingScreen = [""];
+      setItems(LandingScreen);
+    } else if (screen === "Dashboard") {
+      const Dashboard = ["Home", "Settings"];
+      setItems(Dashboard);
+    }
+  }, [screen]);
+
+  const HandleNavigation = (item) => {
+    let new_item = item.replace(/\s+/g, "");
+    navigate(`/${new_item}`);
+  };
+
   return (
     <>
       <div className={`flex p-4 items-center ${backGroundColor}`}>
@@ -58,12 +76,19 @@ const Navbar = ({ updateState }) => {
           </p>
         </div>
         <div className="ml-auto flex space-x-2 p-2 items-center text-sm sm:space-x-8 sm:text-lg">
-          <p className={`${text} font-bold cursor-pointer`}>Home</p>
-          <p className={`${text} font-bold cursor-pointer`}>Settings</p>
+          {items.map((item) => (
+            <p
+              key={item}
+              className={`${text} font-bold cursor-pointer`}
+              onClick={() => HandleNavigation(item)}
+            >
+              {item}
+            </p>
+          ))}
           <img src={userPhoto} alt={``} className="w-8 h-8 cursor-pointer" />
           <img
             src={imageSet}
-            alt={``} 
+            alt={``}
             className="w-8 h-8 cursor-pointer"
             onClick={() => handleLightClickListener()}
           />
