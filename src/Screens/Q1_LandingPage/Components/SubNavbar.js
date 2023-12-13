49fea29b44357140f650a67fda5ee2f80bf2022b
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-14 00:11:25 +0530

    Integrated the modal on login, Register button

--- src/Screens/Q1_LandingPage/Components/SubNavbar.js
+++ src/Screens/Q1_LandingPage/Components/SubNavbar.js
@@ -1,11 +1,14 @@
 import BackGround1 from "../../Icons/vector-5.svg";
 import actor from "../../Icons/actor1.png";
 import { useState, useEffect } from "react";
+import CredModal from "./CredModal";
 
 const SubNavbar = ({ getState }) => {
   const [bg, setBg] = useState("bg-steelblue-200");
   const [bg1, setBg1] = useState("text-blue-400");
   const [state, setState] = useState(getState);
+  const [action, setAction] = useState("");
+  const [modal, showModal] = useState(false);
 
   useEffect(() => {
     setState(getState);
@@ -21,38 +24,56 @@ const SubNavbar = ({ getState }) => {
     }
   }, [state]);
 
+  const pass = (value) => {
+    const act = ["Login", "Register"];
+    setAction(act[value]);
+    showModal(true);
+  };
   return (
     <>
-        <div className="relative w-full">
-          <div
-            className={`${bg} text-left text-2xl text-black font-base flex items-center justify-center z-10 sm:h-full`}
-          >
-            <img src={BackGround1} alt={`Background`} className="h-full" />
-          </div>
-          <div className={`absolute w-full h-full inset-0 z-20`}>
-            <div className={`flex justify-evenly items-center p-4`}>
-              <div>
-                <p
-                  className={`sm:text-5xl text-2xl ${bg1} rounded-t-2xl p-2 bg-opacity-40 font-bold `}
+      <div className="relative w-full">
+        <div
+          className={`${bg} text-left text-2xl text-black font-base flex items-center justify-center z-10 sm:h-full`}
+        >
+          <img src={BackGround1} alt={`Background`} className="h-full" />
+        </div>
+        <div className={`absolute w-full h-full inset-0 z-20`}>
+          <div className={`flex justify-evenly items-center p-4`}>
+            <div>
+              <p
+                className={`sm:text-5xl text-2xl ${bg1} rounded-t-2xl p-2 bg-opacity-40 font-bold `}
+              >
+                Welcome to Quiz Quotient
+              </p>
+              <p
+                className={`sm:text-3xl text-lg ${bg1} rounded-b-2xl p-2 bg-opacity-40 font-bold `}
+              >
+                An interactive and fun way to participate in quiz
+              </p>
+              <div className={`flex sm:flex-row flex-col justify-evenly`}>
+                <button
+                  className={`sm:p-3 p-1 bg-pink-500 rounded-xl font-bold text-white`}
+                  onClick={() => pass(0)}
                 >
-                  Welcome to Quiz Quotient
-                </p>
-                <p
-                  className={`sm:text-3xl text-lg ${bg1} rounded-b-2xl p-2 bg-opacity-40 font-bold `}
+                  Log In
+                </button>
+                <button
+                  className={`sm:p-3 p-1 bg-purple-500 rounded-xl font-bold text-white mt-2 sm:mt-0`}
+                  onClick={() => pass(1)}
                 >
-                  An interactive and fun way to participate in quiz
-                </p>
-                <div className={`flex sm:flex-row flex-col justify-evenly`}>
-                  <button className={`sm:p-3 p-1 bg-pink-500 rounded-xl font-bold text-white`}>Log In</button>
-                  <button className={`sm:p-3 p-1 bg-purple-500 rounded-xl font-bold text-white mt-2 sm:mt-0`}>Register</button>
-                </div>
+                  Register
+                </button>
               </div>
-              <div className={`block`}>
-                <img src={actor} alt={`actor`} className={`-z-20`} />
-              </div>
+            </div>
+            <div className={`block`}>
+              <img src={actor} alt={`actor`} className={`-z-20`} />
+            </div>
+            <div>
+              {modal && <CredModal action={action} getStatus={showModal} />}
             </div>
           </div>
         </div>
+      </div>
     </>
   );
 };
