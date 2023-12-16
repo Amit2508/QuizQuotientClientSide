bfe141735a49209176027f6f24300993a7a8eb9c
kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-17 02:51:45 +0530

    Added modal for test info

--- src/Screens/Q2_Dashboard/Components/TestUpcoming.js
+++ src/Screens/Q2_Dashboard/Components/TestUpcoming.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import TestInfoModal from "./ModalTestInfo";
 
 const TestUpcoming = ({ getState }) => {
   let myList = ["bg-pink-600", "bg-red-500", "bg-pink-400", "bg-orange-500"];
   const [state, setState] = useState(getState);
   const [bgColor, setbgColor] = useState("bg-white");
   const [text, setText] = useState("text-black");
-  const [shadow, setShadow] = useState("shadow-sky-500")
+  const [shadow, setShadow] = useState("shadow-sky-500");
+
+  const [Modal, ShowModal] = useState(false);
   useEffect(() => {
     setState(getState);
   }, [getState]);
@@ -14,16 +17,26 @@ const TestUpcoming = ({ getState }) => {
     if (state === 0) {
       setbgColor("bg-white");
       setText("text-black");
-      setShadow("shadow-sky-500")
+      setShadow("shadow-sky-500");
     } else if (state === 1) {
       setbgColor("bg-black");
       setText("text-white");
-      setShadow("shadow-purple-500")
+      setShadow("shadow-purple-500");
     }
   }, [state]);
+
+  const activateModal = () => {
+    if (Modal === false) {
+      ShowModal(true);
+    } else {
+      ShowModal(false);
+    }
+  };
   return (
     <>
-      <div className={`inline-block p-4 m-2 shadow-xl rounded-xl ${shadow} ${bgColor}`}>
+      <div
+        className={`inline-block p-4 m-2 shadow-xl rounded-xl ${shadow} ${bgColor} z-50`}
+      >
         <div>
           <p className={`font-bold ${text}`}>Tests Upcoming</p>
         </div>
@@ -33,17 +46,26 @@ const TestUpcoming = ({ getState }) => {
               0
             )} h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-pink-600 hover:shadow-pink-700 hover:cursor-pointer`}
           >
-            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
-            <p className="z-10 font-bold text-white">Test1</p>
+            <div
+              className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"
+              onClick={() => activateModal()}
+            ></div>
+            <p className="z-10 font-bold text-white onClick={()=>activateModal()">
+              Test1
+            </p>
           </div>
           <div
             className={`${myList.at(
               1
             )} h-32 flex items-center justify-center shadow-lg rounded-xl relative shadow-red-500 hover:shadow-red-600 hover:cursor-pointer`}
           >
-            <div className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"></div>
+            <div
+              className="absolute inset-0 bg-opacity-50 bg-gray-500 rounded-xl"
+              onClick={() => activateModal()}
+            ></div>
             <p className="z-10 font-bold text-white">Test2</p>
           </div>
+          <div>{Modal && <TestInfoModal modalOpen={ShowModal} />}</div>
         </div>
       </div>
     </>
