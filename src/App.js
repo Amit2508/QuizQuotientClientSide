ae96023d5853b0d82dad61bf93eb7a589d3e471b
kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-16 23:59:08 +0530

    Quiz Routes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Screens/Q1_LandingPage/LandingPage";
 import Dashboard from "./Screens/Q2_Dashboard/Dashboard";
+import QuizScreen from "./Screens/Q3_QuizScreen/QuizScreen";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route
-            exact
-            path="/*"
-            element={<LandingPage />}
-          ></Route>
-          <Route
-            exact
-            path="/dashboard"
-            element={<Dashboard />}
-          ></Route>
+          <Route exact path="/*" element={<LandingPage />}></Route>
+          <Route exact path="/dashboard" element={<Dashboard />}></Route>
+          <Route exact path="/quiz" element={<QuizScreen />}></Route>
         </Routes>
       </Router>
     </div>
