26e4e0ddf90a2d6a928b3029cf182108770eb5db
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-17 18:06:31 +0530

    Added Webstate

--- src/Screens/Q3_QuizScreen/QuizScreen.js
+++ src/Screens/Q3_QuizScreen/QuizScreen.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
+import QuestionStatusHolder from "./Components/QuestionStatusHolder";
+
 const QuizScreen = () => {
   let stateVal = localStorage.getItem("web_state");
   if (stateVal === null) {
@@ -7,53 +9,39 @@ const QuizScreen = () => {
   }
   const [state, setState] = useState(parseInt(stateVal));
   const [background, setBackGround] = useState("bg-white");
-  const [text, setText] = useState('text-black');
+  const [text, setText] = useState("text-black");
+
+  const updateState = (state) => {
+    if (state === 0) {
+      setState(1);
+    }
+    if (state === 1) {
+      setState(0);
+    }
+  };
 
   useEffect(() => {
     if (state === 0) {
       setBackGround("bg-white");
-      setText('text-black');
+      setText("text-black");
     } else {
       setBackGround("bg-slate-800");
-      setText('text-white');
+      setText("text-white");
     }
   }, []);
 
-  const generate_question_function = () => {
-    const questions_pentagon = [];
-
-    for (let i = 0; i < 14; i++) {
-      questions_pentagon.push(
-        <div
-          key={i}
-          className="p-1 w-20 bg-yellow-500 cursor-pointer rounded-xl sm:p-3"
-        >
-            <p className={`text-sm`}>Q-{i+1}</p>
-        </div>
-      );
-    }
-    return questions_pentagon;
-  };
-
   return (
     <>
-      <div className={`${background} h-screen`}>
+      <div className={`${background}`}>
         <div>
-            <Navbar/>
+          <Navbar updateState={updateState} />
         </div>
-
-        <div className={`flex h-screen`}>
-          <div className={`p-2 sm:p-4 border border-gren-600 rounded-xl h-screen`}>
-            <div className={`mt-10`}>
-                <p className={`${text} p-2`}>Questions</p>
-            </div>
-            <div className={`grid sm:grid-cols-3 sm:gap-4 gap-1 overflow-y-auto h-96 `}>
-              {generate_question_function()}
-            </div>
+        <div className={`flex justify-evenly`}>
+          <div>
+            <QuestionStatusHolder state={state}/>
           </div>
-
-          <div className={`w-full`}>
-            <p className={`${text} p-2`}>Question Space</p>
+          <div>
+            <p>This is the Question Info Holder</p>
           </div>
         </div>
       </div>
