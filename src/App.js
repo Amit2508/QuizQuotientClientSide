873ed4f92c25e9b6f225f02fea8d394ca92af44d
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-18 16:07:58 +0530

    added the route for quiz result

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Screens/Q1_LandingPage/LandingPage";
 import Dashboard from "./Screens/Q2_Dashboard/Dashboard";
 import QuizScreen from "./Screens/Q3_QuizScreen/QuizScreen";
+import QuizResult from "./Screens/Q4_QuizResult/QuizResult";
 function App() {
-   const [getState, setGetState] = useState(() => {
-    const storedState = parseInt(localStorage.getItem('web_state'));
+  const [getState, setGetState] = useState(() => {
+    const storedState = parseInt(localStorage.getItem("web_state"));
     return isNaN(storedState) ? 0 : storedState;
   });
 
   useEffect(() => {
     document.documentElement.style.backgroundColor =
-      getState === 0 || isNaN(getState) ? '#FFF' : '#1e293b';
+      getState === 0 || isNaN(getState) ? "#FFF" : "#1e293b";
   }, [getState]);
-  
 
   return (
     <div className="App">
@@ -23,6 +23,7 @@ function App() {
           <Route exact path="/*" element={<LandingPage />}></Route>
           <Route exact path="/dashboard" element={<Dashboard />}></Route>
           <Route exact path="/quiz" element={<QuizScreen />}></Route>
+          <Route exact path="/quiz/result" element={<QuizResult />}></Route>
         </Routes>
       </Router>
     </div>
