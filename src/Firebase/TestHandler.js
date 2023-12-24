4443db4eaaf4fe252378efcb97ad5666e882b9b0
= <=>
2023-12-24 19:37:15 +0530

    Implemented the answer Submit button

--- src/Firebase/TestHandler.js
+++ src/Firebase/TestHandler.js
@@ -8,6 +8,8 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import { db } from "./config";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 export async function Reterieve_question(document) {
   const QuestionCollectionRef = collection(db, "Tests"); // 'Tests' is the collection name
@@ -39,6 +41,43 @@ export async function Reterieve_question(document) {
   }
 }
 
-export async function SaveAnswers(tesid, answerList){
-    
+export async function SaveAnswers(testid, answerList) {
+  const token = Cookies.get("ACCESS_TOKEN");
+  if (token.substring(0, 1) !== "{") {
+    const decoded = jwtDecode(token);
+    const email = decoded.email;
+
+    const AnswerCollectionRef = collection(
+      db,
+      "GoogleUser", email,"TestsGiven"
+    );
+    const PaperCollectionRef = collection(
+      db,
+      "Tests",testid,"GoogleUser"
+    );
+
+    const AnswerDocumentRef = doc(AnswerCollectionRef, testid);
+
+    const PaperDocumentRef = doc(PaperCollectionRef, email);
+
+    try {
+      const AnswerDocumentSnapshot = await getDoc(AnswerDocumentRef);
+
+      if (AnswerDocumentSnapshot.exists()) {
+        alert("You have already given the test");
+      } else {
+        const data = {
+          marks: "Not yet announced",
+          answers: answerList,
+          rank: "Not yet announced",
+        };
+
+        await setDoc(AnswerDocumentRef, data);
+        await setDoc(PaperDocumentRef, data);
+        alert("Test Paper Submitted Successfully!!!");
+      }
+    } catch (error) {
+      alert("Internet connection weak, please try again after some time");
+    }
+  }
 }
