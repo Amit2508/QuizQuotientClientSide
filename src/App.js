70bb9b8417175744df10552816171c0eafc8f52d
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-12-30 19:40:34 +0530

    Added Payment Gateway Route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Dashboard from "./Screens/Q2_Dashboard/Dashboard";
 import QuizScreen from "./Screens/Q3_QuizScreen/QuizScreen";
 import QuizResult from "./Screens/Q4_QuizResult/QuizResult";
 import Settings from "./Screens/Q2_Dashboard/Components/Settings";
+import PaymentScreen from "./Screens/Q5_PaymentScreen/PaymentScreen";
 function App() {
   const [getState, setGetState] = useState(() => {
     const storedState = parseInt(localStorage.getItem("web_state"));
@@ -26,6 +27,7 @@ function App() {
           <Route exact path="/quiz" element={<QuizScreen />}></Route>
           <Route exact path="/quiz/result" element={<QuizResult />}></Route>
           <Route exact path="/settings" element={<Settings/>}></Route>
+          <Route exact path="/PaymentGateway" element={<PaymentScreen/>}></Route>
         </Routes>
       </Router>
     </div>
